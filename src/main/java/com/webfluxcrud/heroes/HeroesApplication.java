1089f018318cd4866afca8693aa09d685276fd80
Marees S <mareesvlpt@gmail.com>
2018-07-25 10:29:55 +0530

    MS | Initial project setup from start.spring.io

---
+++ src/main/java/com/webfluxcrud/heroes/HeroesApplication.java
@@ -0,0 +1,12 @@
+package com.webfluxcrud.heroes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeroesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HeroesApplication.class, args);
+	}
+}
